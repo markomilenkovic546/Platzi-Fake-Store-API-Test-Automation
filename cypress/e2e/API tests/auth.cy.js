9eac3db47be36a2cddb28b7a0c8a53f9c85e24a6
markomilenkovic546 <markomilenkovic546@gmail.com>
2023-06-16 10:36:48 +0200

    tests which cover auth group of requests are done

---
+++ cypress/e2e/API tests/auth.cy.js
@@ -0,0 +1,121 @@
+const tv4 = require("tv4");
+
+// Get all users
+describe('Tests which cover "POST, /api/v1/auth/login" request ', function () {
+  it.only("Response status code should be equal 201'", function () {
+    // Create profile
+    cy.createProfile().then((response) => {
+      const loginPostRequestBody = {
+        email: response.body.email,
+        name: response.body.name,
+      };
+
+      // Login to profile
+      cy.request({
+        method: "POST",
+        url: "/api/v1/auth/login/",
+        body: loginPostRequestBody,
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(201);
+      });
+    });
+  });
+
+  it.only("Should validate the actual response against the schema'", function () {
+    cy.createProfile().then((response) => {
+      const loginPostRequestBody = {
+        email: response.body.email,
+        name: response.body.name,
+      };
+      // Login to profile
+      cy.request({
+        method: "POST",
+        url: "/api/v1/auth/login/",
+        body: loginPostRequestBody,
+        failOnStatusCode: false,
+      }).then((response) => {
+        cy.fixture("auth/login-response-schema.json").then((schema) => {
+          // Validate the response body against the schema
+          const isValid = tv4.validate(response.body, schema);
+          expect(isValid).to.be.true;
+        });
+      });
+    });
+  });
+
+  // Get user with session
+  describe('Tests which cover "GET, /api/v1/auth/profile" request ', function () {
+    it.only("Should validate the actual response against the schema'", function () {
+      cy.createProfile().then((response) => {
+        // Login to profile
+        cy.login(response.body.email, response.body.name).then((response) => {
+          const access_token = response.body.access_token;
+          // Get user with session
+          cy.request({
+            method: "GET",
+            url: "/api/v1/auth/profile",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${access_token}`,
+            },
+            failOnStatusCode: false,
+          }).then((response) => {
+            cy.fixture("auth/get-user-schema.json").then((schema) => {
+              // Validate the response body against the schema
+              const isValid = tv4.validate(response.body, schema);
+              expect(isValid).to.be.true;
+            });
+          });
+        });
+      });
+    });
+  });
+
+  it.only("Should validate the actual response against the schema'", function () {
+    cy.createProfile().then((response) => {
+      // Login to profile
+      cy.login(response.body.email, response.body.name).then((response) => {
+        const access_token = response.body.access_token;
+        // Get user with session
+        cy.request({
+          method: "GET",
+          url: "/api/v1/auth/profile",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${access_token}`,
+          },
+          failOnStatusCode: false,
+        }).then((response) => {
+          expect(response.body.email).to.be.eq(loginPostRequestBody.email);
+          expect(response.body.name).to.be.eq(loginPostRequestBody.name);
+        });
+      });
+    });
+  });
+});
+
+// Get a new Access Token with a Refresh Token
+describe('Tests which cover "POST, /api/v1/auth/refresh-token" request ', function () {
+  it.only("Should validate the actual response against the schema'", function () {
+    cy.createProfile().then((response) => {
+      // Login to profile
+      cy.login(response.body.email, response.body.name).then((response) => {
+        const refresh_token = response.body.refresh_token;
+        const postRequestBody = {
+          refreshToken: `${refresh_token}`,
+        };
+        // Get access token by posting refresh token
+        cy.request({
+          method: "POST",
+          url: "/api/v1/auth/refresh-token",
+          body: postRequestBody,
+          failOnStatusCode: false,
+        }).then((response) => {
+          expect(typeof response.body.access_token).to.equal("string");
+          expect(typeof response.body.refresh_token).to.equal("string");
+        });
+      });
+    });
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,32 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Create profile
+Cypress.Commands.add("createProfile", () => {
+  const postRequestBody = {
+    name: Math.random().toString(5).substring(2),
+    email: Math.random().toString(5).substring(2) + "@gmail.com",
+    password: "12345",
+    avatar: "https://api.lorem.space/image/face?w=640&h=480&r=867",
+  };
+  // Create a profile
+  cy.request({
+    method: "POST",
+    url: "/api/v1/users/",
+    body: postRequestBody,
+    failOnStatusCode: false,
+  });
+});
+
+
+Cypress.Commands.add("login", (email, name) => {
+    const loginPostRequestBody = {
+          email: email,
+          name: name
+        };
+        // Login to account
+        cy.request({
+          method: "POST",
+          url: "/api/v1/auth/login/",
+          body: loginPostRequestBody,
+          failOnStatusCode: false,
+        })
+  });
+  
